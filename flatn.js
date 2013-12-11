2c97c871085d20552a628e7fdad32e7b9bfba295
Corey Wagehoft <corey@domandtom.com>
2013-12-11 11:57:18 -0600

    Woohoo we can convert shit and move shit

--- flatn.js
+++ flatn.js
@@ -6,8 +6,11 @@
  * create a thumbnail using GD and another "large file version"
  *
  * ## DEPENDANCIES ##
- * ImageMagick
- * 
+ *  - ImageMagick
+ *  -- Mac: brew install imagemagick
+ *  -- Windows: http://www.imagemagick.org/script/binary-releases.php#windows
+ *  -- Ubuntu: apt-get install imagemagick
+ *  -- CentOS: yum install imagemagick
  */
 
 // Uploads Directory Absolute Path
@@ -26,19 +29,36 @@ var thumb_height = 125;
  */
 var fs = require('fs');
 var im = require('imagemagick');
-var log = require('winston');
+var winston = require('winston');
 var path = require('path');
 var async = require('async');
 eval(fs.readFileSync('readdir.js')+'');
 
 var customColors = {
-	success: 'green',
-	info: 'blue',
-	warn: 'yellow',
-	error: 'red'
+	levels: {
+
+	},
+	colors: {
+		success: 'green',
+		info: 'blue',
+		warn: 'yellow',
+		error: 'red'
+	}
 };
 
-log.addColors(customColors);
+/**
+ * Setting up logging for console and to a file for later review if needed
+ */
+
+var log = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)({colorize: true}),
+		new (winston.transports.File)({ filename: 'flatn.log', level: 'error'})
+	]
+});
+
+// Color Support for Logging?
+winston.addColors(customColors.colors);
 
 /*
  * Scoped Variableds
@@ -47,13 +67,15 @@ var files = [];
 
 async.series([
 
-
 	function(next_step) {
 
 		/**
 		 * Recursively read back through all of the directories and returns an
 		 * array of files into the files var.
 		 */
+		
+		log.info('Reading Files...');
+
 		readDirectory(base_path, 1, function(callback) {
 			files = callback.files;
 			next_step();
@@ -64,11 +86,13 @@ async.series([
 	function(next_step) {
 	// Make the thumbs directory if it does not exist
 		if(thumbs_dir) {
-			
+
+			log.info('Checking for Thumbs Directory..');
 			fs.exists(thumbs_dir,function(exists) {
 
 				if(!exists) {
 					fs.mkdirSync(thumbs_dir);
+					log.info('Thumbs Directory Created');
 				}
 				next_step();
 			});
@@ -78,13 +102,64 @@ async.series([
 		}
 	},
 
+	// Remove files like .DS_Store and Thumbs.db
+	function(next_step) {
+
+		log.info('Checking for files we need to remove..');
+
+		async.eachSeries(files, function(file,next_each) {
+
+			// Gets the filename from the absolute path;
+			var filename = file.replace(/^.*[\\\/]/, '');
+
+			if(filename == 'Thumbs.db' || filename == '.DS_Store' || filename == 'Thumb.db') {
+
+				fs.unlink(file, function(err) {
+
+					if(err) {
+						log.error('Error Removing File: ' + err + ' File: ' + file);
+						next_each();
+					} else {
+						log.info('Removed Unneeded File: ' + file);
+
+						// Now after we have removed the unneeded file
+						// Lets remove it from the files array so that 
+						// imagemagick does not try to convert it
+						
+						var index = files.indexOf(file);
+
+						if(index > -1) {
+							files.splice(index, 1);
+						}
+
+						next_each();
+					}
+				});
+
+			} else {
+				next_each();
+			}
+
+		}, function(err) {
+			if(err) {
+				log.error('Error Removing Files: ' + err);
+			}
+
+			next_step();
+		});
+	},
+
 	function(next_step) {
 		// Loop through all of the returned files to move them and create
 		// thumbs if we want them
 		// We force this to be syncronous as to not overload the cpu with all
 		// of the GD
-		async.eachSeries(files, function(file) {
+		
+		console.log('');
+		log.info('Processing image thumbnails & moving images..');
+		console.log('');
 
+		async.eachSeries(files, function(file,next_each) {
 
 			// Get the filename out of the absolute path
 			var filename = file.replace(/^.*[\\\/]/, '');
@@ -93,7 +168,7 @@ async.series([
 			async.series([
 
 				// Resize the images if we have a thumbs directory
-				function(next_step) {
+				function(inner_next_step) {
 
 					if(thumbs_dir) {
 
@@ -103,6 +178,7 @@ async.series([
 						im.identify(file, function(err,features) {
 
 							if(features && (features.format == 'JPEG' || features.format == 'PNG')) {
+
 								// Image magick resizing
 								im.resize({
 									srcPath: file,
@@ -112,50 +188,69 @@ async.series([
 								}, function(err, stdout, stderr) {
 
 									if(!err) {
-										log.success('Resize Successful: ' + filename);
-										next_step();
+										log.info('Resize Successful: ' + filename);
+										inner_next_step();
 									} else {
 										log.error('Error: ' + err);
-										next_step(err);
+										inner_next_step(err);
 									}
 
 									if(stderr) {
 										log.error('stderr: ' + stderr);
-										next_step(err);
+										inner_next_step(err);
 									}
 								});
 							} else {
 
-								next_step();
+								inner_next_step();
 							}
 						});
-						
 
 					} else {
-						next_step();
+						inner_next_step();
 					}
 				},
 				// Move the image to the flatn_to dir
-				function(next_step) {
+				function(inner_next_step) {
 
+					// Create the to absolute path
 					var to = flatn_to + '/' + filename;
-					console.log(to);
 
+					// rename is how to move files with nodejs
 					fs.rename(file, to, function(cb) {
-						if(!cb) {
-							next_step();
+						if(cb) {
+							inner_next_step('Error Moving File: ' + file);
+
 						} else {
-							next_step('Error Moving File: ' + file);
+							inner_next_step();
 						}
 					});
+				},
+
+				// Create another copy of the original file with the -large extension to it
+				function(inner_next_step) {
+
+					// Create the to absolute path
+					var file_path = flatn_to + '/' + filename;
+					var file_sliced = filename.slice(0, -4);
+					var extension = (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) : undefined;
+
+					// Now lets copy this file
+					fs.createReadStream(file_path)
+					.pipe(fs.createWriteStream(flatn_to + '/' + file_sliced + '-large.' + extension));
+
+					log.info('Large version created: ' + filename);
+
+					inner_next_step();
 				}
+
 			], function(err) {
 				if(!err) {
 					log.info('File Moved: ' + filename);
-					next_step();
+					next_each();
 				} else {
 					log.error(err);
-					next_step();
+					next_each();
 				}
 			});
 
@@ -163,10 +258,17 @@ async.series([
 			if(err) {
 				log.error('ERROR');
 			}
+			next_step();
 		});
 
-		next_step();
+	}], function(err) {
+
+	if(err) {
+		log.error(err);
+	} else {
+		console.log('');
+		log.info('Images Processed: ' + files.length);
+		log.info(' #### DONE WITH FLATN ####');
+		console.log('');
 	}
-], function() {
-	log.info(' #### DONE WITH FLATN ####');
 });
